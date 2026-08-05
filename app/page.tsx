94b55419b63637db30b05b2d481c8ad0c7caf166
38ed-ma <97fcbrys4j@privaterelay.appleid.com>
2026-08-05 04:53:37 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+"use client"
+
+import { useState } from "react"
+
 export default function Home() {
+  const [login, setLogin] = useState(false)
+
   return (
     <main>
       <h1>نايس سيتي</h1>
       <p>وزارة العدل الإلكترونية</p>
 
-      <button>
+      <button onClick={() => setLogin(true)}>
         تسجيل الدخول (للعدل فقط)
       </button>
 
@@ -14,6 +20,28 @@ export default function Home() {
       <p>
         هنا يتم تقديم الخدمات الإلكترونية الخاصة بالعدل والقضايا والتراخيص.
       </p>
+
+      {login && (
+        <div>
+          <h2>تسجيل دخول العدل</h2>
+
+          <input placeholder="اسم المستخدم" />
+
+          <br />
+
+          <input placeholder="كلمة المرور" type="password" />
+
+          <br />
+
+          <button>
+            دخول
+          </button>
+
+          <button onClick={() => setLogin(false)}>
+            إغلاق
+          </button>
+        </div>
+      )}
     </main>
   )
 }
