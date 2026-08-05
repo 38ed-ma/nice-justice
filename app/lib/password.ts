0cb271461a4b1ae06ddf70a2f9f16205afaaa203
38ed-ma <97fcbrys4j@privaterelay.appleid.com>
2026-08-05 18:48:39 +0300

    Create password.ts

---
+++ app/lib/password.ts
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs"
+
+
+export function encryptPassword(password:string){
+
+return bcrypt.hashSync(password,10)
+
+}
+
+
+
+export function checkPassword(
+password:string,
+encrypted:string
+){
+
+return bcrypt.compareSync(
+password,
+encrypted
+)
+
+}
